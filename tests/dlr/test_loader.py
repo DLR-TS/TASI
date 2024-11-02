edfc3a21962912110b35524cc987ceb318588d50
Klitzke, Lars <lars.klitzke@dlr.de>
2024-11-02 16:10:39 +0100

    fix tests and add loading version 1.1.0

--- tests/dlr/test_loader.py
+++ tests/dlr/test_loader.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from unittest import TestCase
-from tasi.dlr.dataset import DLRUTDatasetManager
+from tasi.dlr.dataset import DLRUTDatasetManager, DLRUTVersion
 
 
 class DLRUTLoadTestCase(TestCase):
@@ -9,15 +9,24 @@ class DLRUTLoadTestCase(TestCase):
     def test_load_v100_version(self):
 
         path = Path("/tmp")
-        manager = DLRUTDatasetManager(DLRUTDatasetManager.VERSIONS.v1_0_0)
+        manager = DLRUTDatasetManager(DLRUTVersion.v1_0_0)
         manager.load(path=path)
 
-        os.path.exists(path.joinpath('DLR-UT_v1-0-0'))
+        self.assertTrue(os.path.exists(path.joinpath('DLR-UT_v1-0-0')))
 
     def test_load_v101_version(self):
 
         path = Path("/tmp")
-        manager = DLRUTDatasetManager(DLRUTDatasetManager.VERSIONS.v1_0_1)
+        manager = DLRUTDatasetManager(DLRUTVersion.v1_0_1)
         manager.load(path=path)
 
-        os.path.exists(path.joinpath('DLR-UT_v1-0-0'))
+        self.assertTrue(os.path.exists(path.joinpath('DLR-UT_v1-0-0')))
+
+    
+    def test_load_v110_version(self):
+        path = Path("/tmp")
+        manager = DLRUTDatasetManager(DLRUTVersion.v1_1_0)
+        manager.load(path=path)
+
+        self.assertTrue(os.path.exists(path.joinpath('DLR-Urban-Traffic-dataset_v1-1-0/')))
+        
