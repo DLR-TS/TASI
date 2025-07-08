75d1fc8561590f55d7ad0591a1ae807de7adb072
Lars Klitzke <lars.klitzke@dlr.de>
2025-07-08 14:08:40 +0200

    Handle dataset manager on proxy error (#21)

--- tasi/io/zenodo.py
+++ tasi/io/zenodo.py
@@ -1,4 +1,6 @@
+import logging
 from enum import Enum
+from typing import Dict, Optional
 
 import requests
 
@@ -12,12 +14,13 @@ class ZenodoConnector:
 
     ZENODO_API: str = "https://zenodo.org/api/records"
 
-    def __init__(self, title: str, parent_id: int = None):
+    def __init__(self, title: str, parent_id: int | None = None):
         """
         Initializes the ZenodoConnector object with a given title.
 
         Args:
             title (str): The title of the dataset to query from Zenodo.
+            parent_id (int, optional): The index of the parent record.
         """
         self.title = title
         self.parent_id = parent_id
@@ -26,14 +29,14 @@ class ZenodoConnector:
         self.versions = self.get_versions()
         self.dois = self.get_dois()
 
-    def get_records(self) -> list[dict]:
+    def get_records(self) -> list[Dict]:
         """
         This method sends a GET request to the Zenodo API and retrieves all
         records matching the given title. The records are then sorted by
         their creation date.
 
         Returns:
-            list[dict]: A list of records from Zenodo, sorted by creation date.
+            list[Dict]: A list of records from Zenodo, sorted by creation date.
 
         Raises:
             requests.exceptions.RequestException: If there is an error with the HTTP request.
@@ -50,69 +53,84 @@ class ZenodoConnector:
         if self.parent_id is not None:
             params["custom"] = f'pid_value:"{self.parent_id}"'
 
-        # query API
-        response = requests.get(self.ZENODO_API, params=params)
+        try:
+            # query API
+            response = requests.get(self.ZENODO_API, params=params)
 
-        if response.status_code == 200:
-            # Extract records from response
-            records = response.json()["hits"]["hits"]
+            if response.status_code == 200:
+                # Extract records from response
+                records = response.json()["hits"]["hits"]
 
-            if not records:
-                raise ValueError(
-                    f"No records found for {self.title=} and {self.parent_id=}"
+                if not records:
+                    raise ValueError(
+                        f"No records found for {self.title=} and {self.parent_id=}"
+                    )
+
+                return records
+            else:
+                raise requests.exceptions.RequestException(
+                    f"Error retrieving data. Status code: {response.status_code}"
                 )
-
-            return records
-        else:
-            raise requests.exceptions.RequestException(
-                f"Error retrieving data. Status code: {response.status_code}"
+        except requests.exceptions.ProxyError as err:
+            logging.warning(
+                "Failed to fetch datasets because of a proxy connection error. "
+                + "Ensure that the proxy is properly configured and allows accessing zenodo.org"
             )
+            return []
 
-    def get_versions(self) -> dict[str]:
+    def get_versions(self) -> Dict[str, int]:
         """
         This method extracts the version information from each record and
         returns it as a dictionary, along with the latest version.
 
         Returns:
-            dict[str]: A dictionary of versions with 'v' prefixed and version numbers as keys,
-                       including the latest version.
+            Dict[str, int]: A dictionary of versions with 'v' prefixed and
+                            version numbers as keys, including the latest version.
         """
         # Get the latest version
-        latest_version = {"latest": "v" + self.records[0]["metadata"]["version"]}
+        if self.records:
 
-        # Get all other versions
-        versions = {
-            "v"
-            + record["metadata"]["version"].replace(".", "_"): "v"
-            + record["metadata"]["version"]
-            for record in self.records
-        }
+            latest_version = {"latest": "v" + self.records[0]["metadata"]["version"]}
 
-        # Return all versions
-        return {**latest_version, **versions}
+            # Get all other versions
+            versions = {
+                "v"
+                + record["metadata"]["version"].replace(".", "_"): "v"
+                + record["metadata"]["version"]
+                for record in self.records
+            }
 
-    def get_dois(self) -> dict[str]:
+            # Return all versions
+            return {**latest_version, **versions}
+
+        return {}
+
+    def get_dois(self) -> Dict[str, int]:
         """
         This method extracts the DOI information from each record and maps
         it to its respective version. It also returns the latest DOI.
 
         Returns:
-            dict[str]: A dictionary of DOIs with 'v' prefixed and version numbers as keys,
-                       including the latest DOI.
+            Dict[str, int]: A dictionary of DOIs with 'v' prefixed and version
+                            numbers as keys, including the latest DOI.
         """
-        # Get the latest DOI
-        latest_doi = {"latest": int(self.records[0]["doi"].split(".")[-1])}
+        if self.records:
 
-        # Get all other DOIs
-        dois = {
-            "v" + record["metadata"]["version"]: int(record["doi"].split(".")[-1])
-            for record in self.records
-        }
+            # Get the latest DOI
+            latest_doi = {"latest": int(self.records[0]["doi"].split(".")[-1])}
 
-        # Return all DOIs
-        return {**latest_doi, **dois}
+            # Get all other DOIs
+            dois = {
+                "v" + record["metadata"]["version"]: int(record["doi"].split(".")[-1])
+                for record in self.records
+            }
 
-    def get_version_enum(self) -> Enum:
+            # Return all DOIs
+            return {**latest_doi, **dois}
+
+        return {}
+
+    def get_version_enum(self) -> Optional[Enum]:
         """
         Generates an Enum for the versions.
 
@@ -122,4 +140,5 @@ class ZenodoConnector:
         Returns:
             Enum: An Enum class representing the dataset versions.
         """
-        return Enum(self.title.replace(" ", "") + "Version", self.versions)
+        if self.versions:
+            return Enum(self.title.replace(" ", "") + "Version", self.versions)
